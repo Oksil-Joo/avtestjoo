0ab56c3c3fb421172937f646c0447b8c772935d1
Oksil-Joo <bbkke07@gmail.com>
2022-03-25 16:41:08 -0400

    teststart

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link> 
   </nav>
-  <router-view/>
+  <p class="test">test here is my test area</p>
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primary;
+  margin-top: 60px;
 }
-
+.test {
+  color: $grey;
+}
 nav {
   padding: 30px;
 
